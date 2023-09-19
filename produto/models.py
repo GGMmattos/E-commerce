a672525d5e4d1602b025f427afdcf966765e73ea
Gabriel Matos <ra116843@uem.br>
2023-09-19 14:13:03 -0300

    criação do model produto

--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model): # criação do modelo - TABELA DO BANCO 
+    nome = models.CharField(max_length=255)
+    decricao_curta = models.TextField(max_length=255)
+    descricao_longa = models.TextField
+    imagem = models.ImageField(
+        upload_to='produto_imagem/%Y/&m/', blank=True, null=True)
+    slug = models.SlugField(unique=True)
+    preco = models.FloatField()
