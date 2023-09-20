d7f006e2b0bb2ef372a1ff5fd1c0447942e0f42e
Gabriel Matos <ra116843@uem.br>
2023-09-20 09:43:06 -0300

    importacao da funcao de validacao de CPF

--- pedido/admin.py
+++ pedido/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Pedido, ItemPedido
 
 
-class ItemPedidoInline(admin.TabularInline):
+class ItemPedidoInline(admin.TabularInline): #Ao visualizar um pedido verifica todos os itens deste pedido
     model = ItemPedido
     extra = 1
 
@@ -10,8 +10,6 @@ class PedidoAdmin(admin.ModelAdmin):
     inlines = [
         ItemPedidoInline
     ]
-
-#Ao visualizar um pedido verifica todos os itens deste pedido
 
 # Register your models here.
 admin.site.register(Pedido, PedidoAdmin)
