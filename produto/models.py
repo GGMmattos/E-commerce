8e745e1d7d9f61031828f6c4e9b0167fa9164b7f
Gabriel Matos <ra116843@uem.br>
2023-09-20 14:04:58 -0300

    up

--- produto/models.py
+++ produto/models.py
@@ -7,7 +7,7 @@ from django.conf import settings
 class Produto(models.Model): # criação do modelo - TABELA DO BANCO 
     nome = models.CharField(max_length=255)
     decricao_curta = models.TextField(max_length=255)
-    descricao_longa = models.TextField
+    descricao_longa = models.TextField()
     imagem = models.ImageField(
         upload_to='produto_imagem/%Y/&m/', blank=True, null=True)
     slug = models.SlugField(unique=True)
