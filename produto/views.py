1cabccca230b87b00031da0c63c7ce06af83bf23
Gabriel Matos <ra116843@uem.br>
2023-09-21 20:55:01 -0300

    esta alteração deve ser corrigida

--- produto/views.py
+++ produto/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views import View
 from django.http import HttpResponse
+from . import models
 
 
 # Create your views here.
 
-class ListaProdutos(View):
-    def get(self, *args, **kwargs):
-        return  HttpResponse('ListaProdutos')
+class ListaProdutos(ListView):
+    model =models.Produto
+    template_name = 'produto/lista.html'
 
 class DetalheProduto(View):
     def get(self, *args, **kwargs):
