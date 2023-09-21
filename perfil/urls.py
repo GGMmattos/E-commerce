54268a5f0d1e62ae413e2cfca242024da13a6255
Gabriel Matos <ra116843@uem.br>
2023-09-21 09:14:14 -0300

    cricao de urls

--- perfil/urls.py
+++ perfil/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.Criar.as_View(), name='criar'),
+    path('atualizar/', views.Atualizar.as_View(), name='atualizar'),
+    path('login/', views.Login.as_View(), name='login'),
+    path('logout/', views.Logout.as_View(), name='logout'),
     
 ]
