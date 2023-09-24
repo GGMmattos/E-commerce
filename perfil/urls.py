0308e6d9f62c150b0c6520fad012cdccf8467a55
unknown <mhsfn00@gmail.com>
2023-09-24 14:09:04 -0300

    Criou a página de cadastro e linkou com o botão 'Cadastrar' do _head.html

--- perfil/urls.py
+++ perfil/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'perfil'
 
 urlpatterns = [
-    path('', views.Criar.as_view(), name='criar'),
+    path('cadastro/', views.Cadastro, name='criar'),
     path('atualizar/', views.Atualizar.as_view(), name='atualizar'),
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
--- perfil/views.py
+++ perfil/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.views import View
 from django.http import HttpResponse
+from . import models
 
 
 # Create your views here.
+def Cadastro (request):
+    return render (request, 'perfil/cadastro.html')
+
 class Criar(View):
     def get(self, *args, **kwargs):
         return  HttpResponse('Criar')
-
+    
 class Atualizar(View):
     def get(self, *args, **kwargs):
         return  HttpResponse('Atualizar')
