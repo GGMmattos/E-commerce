f3fbfe7c3c961723b8b0c1c2b99fc0bd17856c98
Gabriel Matos <ra116843@uem.br>
2023-09-23 23:43:38 -0300

    atualizações

--- produto/views.py
+++ produto/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views import View
