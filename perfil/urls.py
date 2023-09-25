b437ab32d2957a482756f8bdcc2fb333318cbbc1
unknown <mhsfn00@gmail.com>
2023-09-25 15:14:50 -0300

    E pra ta funcionando busca/cadastro/login/logout

--- perfil/urls.py
+++ perfil/urls.py
@@ -1,12 +1,14 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from . import views
+from .forms import LoginForm
 
 app_name = 'perfil'
 
 urlpatterns = [
     path('cadastro/', views.Cadastro, name='criar'),
+    path('cadastro-perfil/', views.Cadastro_perfil, name='criarPerfil'),
     path('atualizar/', views.Atualizar.as_view(), name='atualizar'),
-    path('login/', views.Login.as_view(), name='login'),
-    path('logout/', views.Logout.as_view(), name='logout'),
-    
+    path('login/',auth_views.LoginView.as_view(template_name='perfil/entrar.html' ,authentication_form=LoginForm), name='login'),
+    path('logout/', views.Logout, name='logout'),
 ]
--- perfil/views.py
+++ perfil/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.views import View
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth import logout
 from . import models
 from . import forms
 from .forms import SignupForm
@@ -24,21 +26,63 @@ def Cadastro (request):
         'form': form
     })
 
-def Cadastro_perfil (request): #TODO CRIADO PARA TEST RS
-    if request.method == 'POST': 
-        form = PerfilForm(request.POST)
+def Logout(request):
+    logout(request)
+    return redirect('/')
 
-        if form.is_valid(): 
-            form.save()
-            return redirect('/')
-    else:
-        form = PerfilForm() 
+def Cadastro_perfil (request):
+    pass
+#     if request.method == 'POST': 
+#         form = PerfilForm(request.POST)
 
-    return render (request, 'perfil/cadastro.html',{ 
-        'form': form
-    })
+#         if form.is_valid(): 
+#             form.save()
+#             return redirect('/')
+#     else:
+#         form = PerfilForm() 
 
+#     return render (request, 'perfil/cadastroPerfil.html',{ 
+#         'form': form
+#     })
 
+# def MeuPerfil(LoginRequiredMixin, View):
+#     def get(self, request):
+#         user_form = UserUpdateForm(instance=request.user)
+#         profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+#         context = {
+#             'user_form': user_form,
+#             'profile_form': profile_form,
+#         }
+
+#         return render(request, 'perfil/meuPerfil.html')
+    
+#     def post(self, request):
+#         user_form = UserUpdateForm(
+#             request.POST,
+#             instance=request.user
+#         )
+#         profile_form = ProfileUpdateForm(
+#             request.POST,
+#             request.FILES,
+#             instance=request.user.profile
+#         )
+
+#         if user_form.is_valid() and profile_form.is_valid():
+#             user_form.save()
+#             profile_form.save()
+
+#             messages.success(request,'Usuário alterado')
+
+#             return redirect ('/')
+#         else:
+#             context = {
+#                 'user_form' : user_form,
+#                 'profile_form' : profile_form
+#             }
+#             messages.error(request,'Falha ao alterar o usuário')
+
+#             return render(request, 'perfil/meuPerfil.html', context)
     
 class Atualizar(View):
     def get(self, *args, **kwargs):
@@ -48,6 +92,6 @@ class Login(View):
     def get(self, *args, **kwargs):
         return  HttpResponse('Login')
 
-class Logout(View):
-    def get(self, *args, **kwargs):
-        return  HttpResponse('Logout')
+# class Logout(View):
+#     def get(self, *args, **kwargs):
+#         return  HttpResponse('Logout')
